346066a28defd28834bae9709108c5bdc65d3c32
Shea Lewis <shea.lewis89@gmail.com>
2017-02-19 00:18:26 -0800

    Complete collection tests

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -9,19 +9,69 @@ class CollectionTest extends TestCase
 {
     public function test_all_method()
     {
+        $collection = new Collection(['foo', 'bar', 'baz']);
+        $this->assertSame(['foo', 'bar', 'baz'], $collection->all());
+    }
+
+    public function test_get_method()
+    {
+        $collection = new Collection(['foo', 'bar', 'baz']);
+        $this->assertSame('bar', $collection->get(1));
+
+        $collection = new Collection(['foo' => 'bar', 'lorem' => 'ipsum']);
+        $this->assertSame('ipsum', $collection->get('lorem'));
+    }
+
+    public function test_push_method()
+    {
         $collection = new Collection('foo');
-        $this->assertSame(['foo'], $collection->all());
+        $collection->push('bar');
 
-        $collection = new Collection(2);
-        $this->assertSame([2], $collection->all());
+        $this->assertSame(['foo', 'bar'], $collection->all());
+    }
 
-        $collection = new Collection(false);
-        $this->assertSame([false], $collection->all());
+    public function test_put_method()
+    {
+        $collection = new Collection(['lorem' => 'bar']);
+        $collection->put('lorem', 'ipsum');
 
-        $collection = new Collection(null);
-        $this->assertSame([], $collection->all());
+        $this->assertSame(['lorem' => 'ipsum'], $collection->all());
+    }
 
-        $collection = new Collection;
-        $this->assertSame([], $collection->all());
+    public function test_remove_method()
+    {
+        $collection = new Collection(['foo' => 'bar', 'lorem' => 'ipsum']);
+        $collection->remove('lorem');
+
+        $this->assertSame(['foo' => 'bar'], $collection->all());
+    }
+
+    public function test_exists_method()
+    {
+        $collection = new Collection(['foo' => 'bar']);
+
+        $this->assertTrue($collection->exists('foo'));
+        $this->assertFalse($collection->exists('bar'));
+    }
+
+    public function test_count_method()
+    {
+        $collection = new Collection(['foo', 'bar', 'baz', 'lorem', 'ipsum']);
+
+        $this->assertSame(5, $collection->count());
+    }
+
+    public function test_first_method()
+    {
+        $collection = new Collection(['foo', 'bar', 'baz']);
+
+        $this->assertSame('foo', $collection->first());
+    }
+
+    public function test_last_method()
+    {
+        $collection = new Collection(['foo', 'bar', 'baz']);
+
+        $this->assertSame('baz', $collection->last());
     }
 }
